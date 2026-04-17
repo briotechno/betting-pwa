ef62270d9a52f022854cb8dd151414cee9177032
chirag <269145775+chirag-work-xi@users.noreply.github.com>
2026-04-18 00:57:24 +0530

    footer fix

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,9 +4,9 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer className="px-5 pt-4 lg:pt-10 pb-32 bg-[#1e1e1e] text-white border-t border-white/5">
-      <div className="max-w-[1240px] mx-auto space-y-7">
+      <div className="max-w-[1240px] mx-auto space-y-5">
         {/* Logo and Intro */}
-        <div className="space-y-4">
+        <div className="space-y-2">
           <div className="flex flex-col gap-1">
             <img
               src="https://www.fairplay247.vip/_nuxt/img/fairplay-website-logo.09a29c5.png"
@@ -23,7 +23,7 @@ export default function Footer() {
         <div className="h-[1px] w-full bg-white/5" />
 
         {/* Disclaimer */}
-        <div className="space-y-3">
+        <div className="space-y-1">
           <h4 className="text-[14px] font-bold text-white">Disclaimer :</h4>
           <p className="text-[12px] leading-[1.8] text-white/80 font-normal">
             Please note that Gambling involves a financial risk and could be addictive over time if not practised within limits. Only 18+ people should use the services and should use it responsibly. Players should be aware of any financial risk and govern themselves accordingly.
@@ -31,7 +31,7 @@ export default function Footer() {
         </div>
 
         {/* Copyright */}
-        <div className="pt-2">
+        <div className="pt-1">
           <p className="text-[12px] text-white/60 font-normal">
             © 2026 fairplay. All Rights Reserved.
           </p>
